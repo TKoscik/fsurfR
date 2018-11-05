c4555ac4bbca7a86e4c350f1fd09d53a98c714ce
TKoscik <timkoscik@gmail.com>
2018-11-05 15:45:25 -0600

    add function

---
+++ R/get.curv.list.R
@@ -0,0 +1,22 @@
+get.curv.list <- function(data.dir,
+                          id.ls,
+                          hemi,
+                          var.name) {
+  outls <- character(length(id.ls))
+  fls <- character(0)
+  for (i in 1:length(data.dir)) {
+    fls <- c(fls, list.files(data.dir[j], pattern=hemi, full.names = TRUE))
+  }
+  fls <- fls[grepl(pattern = var.name, fls)]
+  for (i in 1:length(id.ls)) {
+    fname <- fls[grepl(pattern=id.ls[i], fls)]
+    if (length(fname) == 0) {
+      stop(sprintf("Error: Could not find file for %s", as.character(id.ls[i])))
+    } else if (length(fname) > 1) {
+      stop(sprintf("Error: Found multiple files for %s", as.character(id.ls[i])))
+    } else {
+      outls[i] <- fname
+    }
+  }
+  return(outls)
+}
